eaf55da114e045a5da337de42066e13520c7b300
hello-yuki0409 <bobu2784@gmail.com>
2025-08-07 23:56:00 +0900

    supabaseからデータを撮るところまで完了

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
 import { Button, ChakraProvider, theme } from "@chakra-ui/react";
+import { useEffect } from "react";
+import { GetAllTodos } from "./lib/todo";
 
 export default function App() {
+  useEffect(() => {
+    const getAllTodos = async () => {
+      const todosData = await GetAllTodos();
+      console.log(todosData);
+    };
+    getAllTodos();
+  }, []);
+
   return (
     <div className="App">
       <ChakraProvider theme={theme}>
